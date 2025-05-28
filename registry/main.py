9bbe0d61feed22f6a6ce38d5b49685d81f45dbb8
Kyle Widmann <kyle.widmann@gmail.com>
2025-05-28 13:33:47 -0400

    Separating api auth endoints from API

---
+++ registry/main.py
@@ -0,0 +1,24 @@
+"""
+Miner API entrypoint.
+"""
+
+import hashlib
+from fastapi import FastAPI, Request
+from fastapi.responses import ORJSONResponse
+from registry.router import router as registry_router
+
+
+app = FastAPI(default_response_class=ORJSONResponse)
+app.include_router(registry_router, prefix="/registry", tags=["Registry"])
+app.get("/ping")(lambda: {"message": "pong"})
+
+
+@app.middleware("http")
+async def request_body_checksum(request: Request, call_next):
+    if request.method in ["POST", "PUT", "PATCH"]:
+        body = await request.body()
+        sha256_hash = hashlib.sha256(body).hexdigest()
+        request.state.body_sha256 = sha256_hash
+    else:
+        request.state.body_sha256 = None
+    return await call_next(request)
