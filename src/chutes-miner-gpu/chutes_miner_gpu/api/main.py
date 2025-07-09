66c14cd1cac80b1ffc2dc4b457b7d87760d45934
Kyle Widmann <kyle.widmann@gmail.com>
2025-07-09 13:33:58 -0400

    Fix auth and RBAC for miner secret

--- src/chutes-miner-gpu/chutes_miner_gpu/api/main.py
+++ src/chutes-miner-gpu/chutes_miner_gpu/api/main.py
@@ -11,3 +11,13 @@ from chutes_miner_gpu.api.config.router import router as config_router
 app = FastAPI(default_response_class=ORJSONResponse)
 app.include_router(config_router, prefix="/config", tags=["Config"])
 app.get("/ping")(lambda: {"message": "pong"})
+
+@app.middleware("http")
+async def request_body_checksum(request: Request, call_next):
+    if request.method in ["POST", "PUT", "PATCH"]:
+        body = await request.body()
+        sha256_hash = hashlib.sha256(body).hexdigest()
+        request.state.body_sha256 = sha256_hash
+    else:
+        request.state.body_sha256 = None
+    return await call_next(request)
